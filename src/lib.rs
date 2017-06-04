68adb262b70d3aa5a5a0d93405f1993104997337
Steven Walter <stevenrwalter@gmail.com>
2017-06-04 17:29:00 -0400

    Actually write to kmsg

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,32 @@
 #[macro_use]
 extern crate slog;
+extern crate slog_async;
 
-use std::io;
+use std::{fs, io};
+use std::cell::RefCell;
+use std::io::Write as IoWrite;
 
 use slog::{Drain, Record, OwnedKVList};
 
+const BUFFER_SIZE : usize = 1024 - 32;
+
 pub struct Kmsg
 {
+    fd: RefCell<fs::File>,
+    buffer: RefCell<[u8; BUFFER_SIZE]>,
+}
+
+impl Kmsg {
+    pub fn new() -> Result<Kmsg, io::Error> {
+        let kmsg = fs::OpenOptions::new()
+            .write(true)
+            .open("/dev/kmsg")?;
+
+        Ok(Kmsg {
+            fd: RefCell::new(kmsg),
+            buffer: RefCell::new([0; BUFFER_SIZE]),
+        })
+    }
 }
 
 impl Drain for Kmsg
@@ -15,13 +35,31 @@ impl Drain for Kmsg
     type Err = io::Error;
 
     fn log(&self, record: &Record, values: &OwnedKVList) -> Result<Self::Ok, Self::Err> {
+        let len = {
+            let mut buf = self.buffer.borrow_mut();
+            let mut cursor = io::Cursor::new(&mut buf[..]);
+            write!(&mut cursor, "{}", record.msg())?;
+            cursor.position() as usize
+        };
+        self.fd.borrow_mut().write_all(&self.buffer.borrow()[..len])?;
         Ok(())
     }
 }
 
 #[cfg(test)]
 mod tests {
+    use super::Kmsg;
+
+    use slog;
+    use slog::Drain;
+
+    use slog_async;
+
     #[test]
     fn it_works() {
+        let drain = Kmsg::new().unwrap().fuse();
+        let drain = slog_async::Async::new(drain).build().fuse();
+        let root = slog::Logger::root(drain, o!());
+        error!(root, "test 123");
     }
 }
